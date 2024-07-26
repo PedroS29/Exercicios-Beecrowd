826a47dbfb08168eafd7b2154a6f68845801d55f
PedroS <pedrosoaresdeassis@gmail.com>
2024-07-25 23:44:58 -0500

    atividade

---
+++ 1008.py
@@ -0,0 +1,10 @@
+idF = int(input())
+
+qntHoras = int(input())
+
+valor = float(input())
+
+salario = qntHoras*valor
+
+print(f'NUMBER = {idF}')
+print(f'SALARY = U$ {salario:.2f}')
---
+++ 1009.py
@@ -0,0 +1,9 @@
+nome = input()
+
+salario = float(input())
+
+mont_vendas = float(input())
+
+tot_vendas = salario + mont_vendas*0.15
+
+print(f'TOTAL = {tot_vendas:.2f}')
