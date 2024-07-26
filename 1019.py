b5edb0fc8883499cd5810b828fdbb3a6d06c1b55
pedro <pedro.assis@sou.ufac.br>
2024-07-26 20:23:19 -0300

    atividade

--- 1019.py
+++ 1019.py
@@ -1,1 +1,13 @@
-N = int(input())
+n = int(input())
+
+hora = 0
+min = 0
+
+while n > 59:
+    min += 1
+    if min > 59:
+        hora += 1
+        min -= 60
+    n -= 60
+
+print(f'{hora}:{min}:{n}')
--- 1021.py
+++ 1021.py
@@ -2,50 +2,52 @@ N = float(input())
 
 print('NOTAS:')
 
-print(f'{N//100} nota(s) de R$ 100.00')
+print(f'{N//100:.0f} nota(s) de R$ 100.00')
 
 N = N % 100
 
-print(f'{N//50} nota(s) de R$ 50.00')
+print(f'{N//50:.0f} nota(s) de R$ 50.00')
 
 N = N % 50
 
-print(f'{N//20} nota(s) de R$ 20.00')
+print(f'{N//20:.0f} nota(s) de R$ 20.00')
 
 N = N % 20
 
-print(f'{N//10} nota(s) de R$ 10.00')
+print(f'{N//10:.0f} nota(s) de R$ 10.00')
 
 N = N % 10
 
-print(f'{N//5} nota(s) de R$ 5.00')
+print(f'{N//5:.0f} nota(s) de R$ 5.00')
 
 N = N % 5
 
-print(f'{N//2} nota(s) de R$ 2.00')
+print(f'{N//2:.0f} nota(s) de R$ 2.00')
 
 N = N % 2
 
 print('MOEDAS:')
 
-print(f'{N:.0f} moeda(s) de R$ 1.00')
+N = N*100
 
-N = N % 0.50
+print(f'{N//100:.0f} moeda(s) de R$ 1.00')
 
-print(f'{N*0.50:.0f} moeda(s) de R$ 0.50')
+N = N % 100
 
-N = N % 0.25
+print(f'{N//50:.0f} moeda(s) de R$ 0.50')
 
-print(f'{N*0.25:.0f} moeda(s) de R$ 0.25')
+N = N % 50
 
-N = N % 0.10
+print(f'{N//25:.0f} moeda(s) de R$ 0.25')
 
-print(f'{N*0.10:.0f} moeda(s) de R$ 0.10')
+N = N % 25
 
-N = N % 0.05
+print(f'{N//10:.0f} moeda(s) de R$ 0.10')
 
-print(f'{N*0.01:.0f} moeda(s) de R$ 0.05')
+N = N % 10
 
-N = N % 0.01
+print(f'{N//5:.0f} moeda(s) de R$ 0.05')
+
+N = N % 5
 
 print(f'{N:.0f} moeda(s) de R$ 0.01')
