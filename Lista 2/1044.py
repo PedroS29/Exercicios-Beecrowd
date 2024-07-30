6b030ad375f122de61655dc597fbff66e4e31112
PedroS29 <132589799+PedroS29@users.noreply.github.com>
2024-07-30 08:19:54 -0500

    Update 1044.py

--- Lista 2/1044.py
+++ Lista 2/1044.py
@@ -1,19 +1,9 @@
 n = input().split()
 
-a = float(n[0])
-b = float(n[1])
-c = float(n[2])
+a = int(n[0])
+b = int(n[1])
 
-if (a < b + c) and (b < a + c) and (c < b + a):
-    if a*a == b*b + c*c:
-        print('TRIANGULO RETANGULO')
-    elif a*a > b*b + c*c:
-        print('TRIANGULO OBTUSANGULO')
-    elif a*a < b*b + c*c:
-        print('TRIANGULO ACUTANGULO')
-    elif a == b == c:
-        print('TRIANGULO EQUILATERO')
-    elif a == b or a == c or b == c:
-        print('TRIANGULO ISOSCELES')
+if a % b == 0 or b % a == 0:
+    print('Sao Multiplos')
 else:
-    print('NAO FORMA TRIANGULO')
+    print('Nao sao Multiplos')
