6d46365a5a901edd33c02b569d4a5f0c33fc22ee
PedroS29 <132589799+PedroS29@users.noreply.github.com>
2024-07-30 08:56:37 -0500

    Update 1046.py

--- Lista 2/1046.py
+++ Lista 2/1046.py
@@ -4,9 +4,8 @@ hi = int(n[0])
 hf = int(n[1])
 
 if hi > hf:
-    duracao = (hf-hi)-24
-    print(f'O JOGO DUROU {abs(duracao)} HORA(S)')
-elif (hi==0) and (hf==0):
+    print(f'O JOGO DUROU {abs((hi-hf)-24)} HORA(S)')
+elif hi==hf:
     print(f'O JOGO DUROU 24 HORA(S)')
 else:
     print(f'O JOGO DUROU {abs(hf-hi)} HORA(S)')
