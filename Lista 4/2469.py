ae9ac795c4fd842a0b4e412e72afd9c335686501
pedro <pedro.assis@sou.ufac.br>
2024-08-09 18:15:23 -0300

    2469 finalizado

--- Lista 4/2469.py
+++ Lista 4/2469.py
@@ -6,13 +6,12 @@ for i in range(n):
     vetor[i] = int(alunos[i])
 
 vetor.sort()
-mais_frequente = vetor[0]
+mais_frequente = vetor.count(vetor[0])
 
 for i in range(n):
     frequencia = vetor.count(vetor[i])
-    if frequencia > mais_frequente:
-        mais_frequente = vetor[i]
+    if frequencia >= mais_frequente:
+        mais_frequente = vetor.count(vetor[i])
+        nummaisfrequente = vetor[i]
 
-print(mais_frequente)
-
-#FINALIZAR
+print(nummaisfrequente)
