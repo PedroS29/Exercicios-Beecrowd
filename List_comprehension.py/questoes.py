1817fd94a2ad0c2ae892f592be9f672054db0137
PedroS29 <pedrosoaresdeassis@gmail.com>
2024-10-10 16:03:54 -0300

    Incompleto

---
+++ List_comprehension.py/questoes.py
@@ -0,0 +1,25 @@
+#Questão 1
+qua_imp = [i*i for i in range(10) if i%2!=0]
+print(qua_imp)
+
+#Questão 2
+sete = [7,17,27,37,47,57,67,87,97,107]
+lista_sete = [i for i in range(108) if i in sete]
+print(lista_sete)
+
+#Questão 3
+frase = ['Exemplo de frase para a questão 3']
+lista_branco = [i+1 for i in frase if i.isspace()==True]
+print(lista_branco)
+
+#Questão 4
+vogais = 'aeiou'
+consoantes = [i for i in frase if i not in 'aeiou']
+print(consoantes)
+#Questão 5
+#Questão 6
+#Questão 7
+#Questão 8
+#Questão 9
+#Questão 10
+#Questão 11
