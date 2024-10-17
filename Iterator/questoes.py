fb137918b6b1a6c6ad711d3113a8cc181de09d2b
PedroS29 <pedrosoaresdeassis@gmail.com>
2024-10-17 16:30:26 -0300

    lista atualizada

--- Iterator/questoes.py
+++ Iterator/questoes.py
@@ -3,7 +3,11 @@ ger = (list((i,i+2) for i in range(6) if i!=3))
 print(f'Resultado da questão 1: {ger}\n')
 
 #questao2
-# ger = 
+def ger2():
+    i = 0
+    while True:
+        yield i
+        i += 1
 
 #questao3
 import random
@@ -11,16 +15,14 @@ ger3 = (list(random.randint(1,60) for _ in range(6)))
 print(f'Resultado da questão 3: {ger3}')
 
 #questao4
-# entrada = int(input('Insira os valores em tuplas: '))
-# ger4 = (sum(i) for i in entrada)
-# for i in ger4:
-#     print(i)
+entrada = [(1, 2, 3), (5, 7), (99, 15, -5)]
+ger4 = (sum(i) for i in entrada)
+lista = []
+for i in ger4:
+    lista.append(i)
+print(*lista)
 
 #questao5
-# entrada2 = iter(input('Insira os elementos: '))
-# for i in entrada2:
-#     if i != i:
-
 
 #questao6
 '''Em questão de velocidade a segunda função é preferível
